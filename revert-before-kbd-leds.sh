cf58c3876e9c0bdcdad86c27cb33b3c56a47f67d
emard <vordah@gmail.com>
2024-04-04 20:38:08 +0200

    revert ROM to a version known to work, before new rom for keyboard LED control

---
+++ revert-before-kbd-leds.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Revert to 4-bit ROM known to work
+
+git checkout 461d7828c0217e3952af005de1d3853dcbc8cb7a -- src/usb_hid_host.v
+git checkout 461d7828c0217e3952af005de1d3853dcbc8cb7a -- src/usb_hid_host/ukp.s
