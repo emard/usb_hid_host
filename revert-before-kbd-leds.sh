42752adad60c3b9d4e3740abfa50b42024dceddb
emard <vordah@gmail.com>
2024-04-05 21:59:07 +0200

    revert script: add reverting asukp

--- revert-before-kbd-leds.sh
+++ revert-before-kbd-leds.sh
@@ -3,3 +3,4 @@
 
 git checkout 461d7828c0217e3952af005de1d3853dcbc8cb7a -- src/usb_hid_host.v
 git checkout 461d7828c0217e3952af005de1d3853dcbc8cb7a -- src/usb_hid_host/ukp.s
+git checkout 461d7828c0217e3952af005de1d3853dcbc8cb7a -- src/usb_hid_host/asukp
